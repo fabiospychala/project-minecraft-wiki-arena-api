229521b07cd319d77b830f2bd5f8ba212caf338d
tmilani-hub <tmilani@edenschool.fr>
2026-05-08 01:28:21 +0200

    V0.0.5

---
+++ js/MAIN.js
@@ -0,0 +1,18 @@
+const $DiaMenu = document.querySelector("#Menu");
+const $BTNmenu = document.querySelector("#BTNmenu");
+const $body = document.querySelector("body");
+const $close = document.querySelector("#close");
+
+let div = document.createElement("div");
+
+$BTNmenu.addEventListener("click", () => {
+    div = document.createElement("div");
+    div.classList.add("voile");
+    $body.appendChild(div);
+    $DiaMenu.showModal();
+});
+
+$close.addEventListener("click", () => {
+    div.remove();
+    $DiaMenu.close();
+});
