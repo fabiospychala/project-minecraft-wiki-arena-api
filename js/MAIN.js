bc9242df4698e7e2e4508292ae4f7ba33263d2c2
tmilani-hub <tmilani@edenschool.fr>
2026-05-12 15:58:18 +0200

    test

--- js/MAIN.js
+++ js/MAIN.js
@@ -1,3 +1,5 @@
+const API = "http://10.69.4.208:3000/v1/";
+
 const $DiaMenu = document.querySelector("#Menu");
 const $BTNmenu = document.querySelector("#BTNmenu");
 const $body = document.querySelector("body");
